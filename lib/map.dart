74ab892a7fdebb9529aa800e01d8f99c97e2e0a7
syj5268 <syj5268@korea.ac.kr>
2022-11-21 03:55:18 +0900

    기본 설정

---
+++ lib/map.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class KakaoMap extends StatefulWidget {
+  const KakaoMap({super.key});
+
+  @override
+  State<KakaoMap> createState() => _KakaoMapState();
+}
+
+class _KakaoMapState extends State<KakaoMap> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('KakaoMap test')),
+        body: Column(
+          children: [],
+        ));
+  }
+}
